b1730c9a9121b46d0420b05a4e521706885efae5
namchid <ndo@trinity.edu>
2015-04-23 20:15:09 -0500

    untested; modified logging in to check for session

--- src/main/scala/com/github/namchid/app/ScalatraTaskManagerServlet.scala
+++ src/main/scala/com/github/namchid/app/ScalatraTaskManagerServlet.scala
@@ -1,43 +1,67 @@
 package com.github.namchid.app
 
+import org.scalatra._
+import scala.xml.{ Text, Node }
+import scalate.ScalateSupport
 import Page.set
 
-class ScalatraTaskManagerServlet extends ScalatraTaskManagerWebAppStack {
+class ScalatraTaskManagerServlet extends ScalatraTaskManagerWebAppStack with FlashMapSupport with ScalateSupport {
 
   get("/") {
-    session.invalidate()
-    val contents = {
-      <form action={ url("/tasks") } method='POST'>
-        <table>
-          <tr>
-            <td class='right'>username</td>
-            <td><input type='text' name='username'/></td>
-          </tr>
-          <tr>
-            <td class='right'>password</td>
-            <td><input type='password' name='password'/></td>
-          </tr>
-          <tr>
-            <td colspan='2' id='centered'><input type='submit'/></td>
-          </tr>
-        </table>
-      </form>
+    (session.get("username"), session.get("password")) match {
+      case (Some(_), Some(_)) =>
+//        redirect("/tasks")
+      case _ =>
+        val contents = {
+          <form action={ url("/tasks") } method='POST'>
+            <table>
+              <tr>
+                <td class='right'>username</td>
+                <td><input type='text' name='username'/></td>
+              </tr>
+              <tr>
+                <td class='right'>password</td>
+                <td><input type='password' name='password'/></td>
+              </tr>
+              <tr>
+                <td colspan='2' id='centered'><input type='submit'/></td>
+              </tr>
+            </table>
+          </form>
+        }
+        set("Scalatra Task Manager Login", contents, "noclass")
     }
-    set("Scalatra Task Manager Login", contents, "noclass")
   }
 
   post("/tasks") {
-    val username = params("username")
-    val password = params("password")
+    (session.get("username"), session.get("password")) match {
+      case (None, None) =>
+        session("username") = params("username")
+        session("password") = params("password") //ok, clearly I shouldn't be storing this. get index later instead
+        val contents = {
+          <h1>You posted:</h1>
+          <h2>{ session("username") }</h2>
+          <h2>{ session("password") }</h2>
+        }
+        set("Scalatra Task Manager", contents, "none")
+      case _ =>
+        redirect("/tasks")
+    }
+  }
+
+  get("/tasks") {
     val contents = {
-      <h1>You posted:</h1>
-      <h2>{ username }</h2>
-      <h2>{ password }</h2>
+      <h1>You already logged in.</h1>
+      <h2>{ session("username") }</h2>
+      <h2>{ session("password") }</h2>
     }
     set("Scalatra Task Manager", contents, "none")
   }
-  
-  
+
+  get("/logout") {
+    session.invalidate()
+    redirect("/")
+  }
 
 }
 
