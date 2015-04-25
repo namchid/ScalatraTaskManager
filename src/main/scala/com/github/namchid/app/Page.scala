ad82cbe76cf7f24ecea72b68460f9bc52a6feb95
namchid <ndo@trinity.edu>
2015-04-24 19:34:23 -0500

    started working on queries for getting tasks

--- src/main/scala/com/github/namchid/app/Page.scala
+++ src/main/scala/com/github/namchid/app/Page.scala
@@ -4,6 +4,12 @@ import org.scalatra._
 import scalate.ScalateSupport
 import scala.xml.{ Text, Node }
 
+import scala.slick.driver.MySQLDriver.simple.Database
+import scala.slick.jdbc.GetResult
+import scala.slick.jdbc.StaticQuery.interpolation
+
+import Tables._
+
 object Page {
 
   val loginHeader = {
@@ -12,6 +18,21 @@ object Page {
     <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Raleway:600,300"/>
     <link rel="stylesheet" type="text/css" href="css/loginStyles.css"/>
     <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+  }
+
+  val tasksHeader = {
+    <title>Scalatra Task Manager</title>
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Raleway:600,300"/>
+    <link rel="stylesheet" type="text/css" href="css/loginStyles.css"/>
+    <link rel="stylesheet" type="text/css" href="css/tasks.css"/>
+    <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+  }
+
+  def getTasks(db: Database, userId: Int) = {
+    db.withSession {
+      implicit session =>
+        //val usersTasks = tasks.filter(i => i.userId == 1)
+    }
   }
 
   def set(contents: Seq[Node], containerClassName: String = "outerContainer") = {
--- src/main/scala/com/github/namchid/app/Tables.scala
+++ src/main/scala/com/github/namchid/app/Tables.scala
@@ -3,21 +3,35 @@ package com.github.namchid.app
 import scala.slick.driver.MySQLDriver.simple._
 import scala.slick.jdbc.GetResult
 
-case class User (
+case class UserCC (
     username: String, 
     password: String,
     userId: Int
 )
 
-class Users(tag: Tag) extends Table[User](tag, "user") {
+case class TaskCC (
+	taskId: Int,
+	userId: Int,
+	description: String
+)
+
+class User(tag: Tag) extends Table[UserCC](tag, "user") {
   def username = column[String]("username")
   def password = column[String]("password")
   def userId = column[Int]("user_id", O.PrimaryKey, O.AutoInc)
   
-  def * = (username, password, userId) <> ((User.apply _).tupled, (User).unapply)
+  def * = (username, password, userId) <> ((UserCC.apply _).tupled, (UserCC).unapply)
+}
+
+class Task(tag: Tag) extends Table[TaskCC](tag, "task") {
+	def taskId = column[Int]("task_id", O.PrimaryKey, O.AutoInc)
+	def userId = column[Int]("user_id")
+	def description = column[String]("description")
+	
+	def * = (taskId, userId, description) <> ((TaskCC.apply _).tupled, (TaskCC.unapply))
 }
 
 object Tables {
-  val users = TableQuery[Users]
-
+  val users = TableQuery[User]
+  val tasks = TableQuery[Task]
 }
